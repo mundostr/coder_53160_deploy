6f1c620faa2d70251d85336c4a86955c6f64bfec
Carlos Perren <idux.net@gmail.com>
2024-09-02 11:04:59 -0300

    Agregado var entorno MONGODB_URI

--- src/config.js
+++ src/config.js
@@ -8,7 +8,8 @@ const config = {
     DIRNAME: path.dirname(new URL(import.meta.url).pathname.replace(/^\/([A-Za-z]:\/)/, '$1')), // Win
     get UPLOAD_DIR() { return `${this.DIRNAME}/public/img` },
     // MONGODB_URI: 'mongodb://127.0.0.1:27017/coder_53160',
-    MONGODB_URI: 'mongodb+srv://coder_53160:coder2024@clustercoder.sxqjiud.mongodb.net/coder_53160',
+    // 'mongodb+srv://coder_53160:coder2024@clustercoder.sxqjiud.mongodb.net/coder_53160'
+    MONGODB_URI: process.env.MONGODB_URI,
     MONGODB_ID_REGEX: /^[a-fA-F0-9]{24}$/,
     SECRET: 'coder_53160_abc1118',
     PRODUCTS_PER_PAGE: 5,
